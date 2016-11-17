0fff02a515940e4d82d9885afa7980c979e5e1bd
Robert Helgesson <robert@chaitsa.com>
2016-11-17 09:01:04 +0100

    Gather known error codes in a wrapper class
    Also add some javadoc and remove the unknown campaign error since it is
    unused.

--- src/main/java/com/clxcommunications/xms/api/ApiError.java
+++ src/main/java/com/clxcommunications/xms/api/ApiError.java
@@ -15,15 +15,52 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 @JsonDeserialize(as = ApiErrorImpl.class)
 public abstract class ApiError {
 
-	public static final String SYNTAX_INVALID_JSON = "syntax_invalid_json";
-	public static final String SYNTAX_INVALID_PARAMETER_FORMAT =
-	        "syntax_invalid_parameter_format";
-	public static final String SYNTAX_CONSTRAINT_VIOLATION =
-	        "syntax_constraint_violation";
-	public static final String UNKNOWN_GROUP = "unknown_group";
-	public static final String UNKNOWN_CAMPAIGN = "unknown_campaign";
-	public static final String CONFLICT_GROUP_NAME = "conflict_group_name";
-	public static final String CONFLICT_SEND_AT = "conflict_send_at";
+	/**
+	 * A collection of known error codes. The REST API reserves the right to
+	 * introduce new error codes as need arise so these may not be exhaustive.
+	 */
+	public static final class Code {
+
+		/**
+		 * The request body is not valid JSON.
+		 */
+		public static final String SYNTAX_INVALID_JSON =
+		        "syntax_invalid_json";
+
+		/**
+		 * The format of a parameter is invalid. For example if an MSISDN is not
+		 * properly formatted.
+		 */
+		public static final String SYNTAX_INVALID_PARAMETER_FORMAT =
+		        "syntax_invalid_parameter_format";
+
+		/**
+		 * The request body doesn’t fulfill all of the constraints set by the
+		 * API. For example missing required parameters or too many elements in
+		 * a list.
+		 */
+		public static final String SYNTAX_CONSTRAINT_VIOLATION =
+		        "syntax_constraint_violation";
+
+		/**
+		 * An unknown group was referenced in the request body.
+		 */
+		public static final String UNKNOWN_GROUP =
+		        "unknown_group";
+
+		/**
+		 * When creating a group with a name that already exists.
+		 */
+		public static final String CONFLICT_GROUP_NAME =
+		        "conflict_group_name";
+
+		/**
+		 * When deleting a message that has already been sent out.
+		 */
+		public static final String CONFLICT_SEND_AT =
+		        "conflict_send_at";
+
+	}
 
 	public abstract String code();
 
