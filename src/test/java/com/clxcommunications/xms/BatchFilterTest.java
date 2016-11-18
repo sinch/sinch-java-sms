0b53ae0a372cacda75b09bf3772000fa3e66ef23
Robert Helgesson <robert@chaitsa.com>
2016-11-18 14:14:30 +0100

    Include start and end dates in batch filter test

--- src/test/java/com/clxcommunications/xms/BatchFilterTest.java
+++ src/test/java/com/clxcommunications/xms/BatchFilterTest.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.threeten.bp.LocalDate;
 
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
@@ -21,11 +22,15 @@ public class BatchFilterTest {
 		        .pageSize(20)
 		        .addFrom("12345", "6789")
 		        .addTag("tag1", "таг2")
+		        .startDate(LocalDate.of(2010, 10, 10))
+		        .endDate(LocalDate.of(2011, 10, 10))
 		        .build();
 
 		String actual = filter.toUrlEncodedQuery(4);
 		String expected = "page=4"
 		        + "&page_size=20"
+		        + "&start_date=2010-10-10"
+		        + "&end_date=2011-10-10"
 		        + "&from=12345%2C6789"
 		        + "&tags=tag1%2C%D1%82%D0%B0%D0%B32";
 
@@ -34,11 +39,14 @@ public class BatchFilterTest {
 
 	@Property(trials = 50)
 	public void generatesValidQueryParameters(int page, int pageSize,
-	        Set<String> from, Set<String> tags) throws Exception {
+	        Set<String> from, Set<String> tags, LocalDate startDate,
+	        LocalDate endDate) throws Exception {
 		BatchFilter filter = ClxApi.buildBatchFilter()
 		        .pageSize(pageSize)
 		        .from(from)
 		        .tags(tags)
+		        .startDate(startDate)
+		        .endDate(endDate)
 		        .build();
 
 		String query = filter.toUrlEncodedQuery(page);
