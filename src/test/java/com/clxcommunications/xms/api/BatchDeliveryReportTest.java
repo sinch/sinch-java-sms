b371a570c6bba78033470ad0e900d1e259c4ed91
Robert Helgesson <robert@chaitsa.com>
2016-11-23 16:35:32 +0100

    Slight expansion of test

--- src/test/java/com/clxcommunications/xms/api/BatchDeliveryReportTest.java
+++ src/test/java/com/clxcommunications/xms/api/BatchDeliveryReportTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import com.clxcommunications.testsupport.TestUtils;
 import com.clxcommunications.xms.ApiObjectMapper;
 import com.clxcommunications.xms.Utils;
+import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
 @RunWith(JUnitQuickcheck.class)
@@ -17,7 +18,7 @@ public class BatchDeliveryReportTest {
 	private final ApiObjectMapper json = new ApiObjectMapper();
 
 	@Test
-	public void canSerialize() throws Exception {
+	public void canSerializeSummary() throws Exception {
 		BatchDeliveryReport input = BatchDeliveryReportImpl.builder()
 		        .batchId(BatchId.of("batchid"))
 		        .totalMessageCount(50)
@@ -58,9 +59,54 @@ public class BatchDeliveryReportTest {
 	}
 
 	@Test
-	public void canDeserialize() throws Exception {
-		BatchDeliveryReport expected = BatchDeliveryReportImpl.builder()
+	public void canSerializeFull() throws Exception {
+		BatchDeliveryReport input = BatchDeliveryReportImpl.builder()
 		        .batchId(BatchId.of("batchid"))
+		        .totalMessageCount(50)
+		        .addStatus(BatchDeliveryReportImpl.Status.builder()
+		                .code(10)
+		                .status(DeliveryStatus.DELIVERED)
+		                .count(20)
+		                .addRecipient("to1", "to2")
+		                .build())
+		        .addStatus(BatchDeliveryReportImpl.Status.builder()
+		                .code(20)
+		                .status(DeliveryStatus.FAILED)
+		                .count(30)
+		                .addRecipient("to3", "to4", "to5")
+		                .build())
+		        .build();
+
+		String expected = Utils.join("\n",
+		        "{",
+		        "  'batch_id': 'batchid',",
+		        "  'total_message_count': 50,",
+		        "  'type': 'delivery_report_sms',",
+		        "  'statuses': [",
+		        "    {",
+		        "      'code': 10,",
+		        "      'status': 'Delivered',",
+		        "      'count': 20,",
+		        "      'recipients': [ 'to1', 'to2' ]",
+		        "    },",
+		        "    {",
+		        "      'code': 20,",
+		        "      'status': 'Failed',",
+		        "      'count': 30,",
+		        "      'recipients': [ 'to3', 'to4', 'to5' ]",
+		        "    }",
+		        "  ]",
+		        "}").replace('\'', '"');
+
+		String actual = json.writeValueAsString(input);
+
+		assertThat(actual, is(TestUtils.jsonEqualTo(expected)));
+	}
+
+	@Property
+	public void canDeserialize(String batchId) throws Exception {
+		BatchDeliveryReport expected = BatchDeliveryReportImpl.builder()
+		        .batchId(BatchId.of(batchId))
 		        .totalMessageCount(50)
 		        .addStatus(BatchDeliveryReportImpl.Status.builder()
 		                .code(10)
