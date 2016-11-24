24bd26beab274fef0f219423dcd6de9856877edf
Robert Helgesson <robert@chaitsa.com>
2016-11-24 14:23:51 +0100

    Generate class containing version information

---
+++ src/main/java-templates/com/clxcommunications/xms/Version.java
@@ -0,0 +1,9 @@
+package com.clxcommunications.xms;
+
+public final class Version {
+
+	public static final String GROUP_ID = "${project.groupId}";
+	public static final String ARTIFACT_ID = "${project.artifactId}";
+	public static final String VERSION = "${project.version}";
+
+}
