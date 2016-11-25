4bcfe09fba9734c45b565ba1e1848b74e7f432e0
Robert Helgesson <robert@chaitsa.com>
2016-11-25 12:37:29 +0100

    Add a bit of javadoc

--- src/main/java-templates/com/clxcommunications/xms/Version.java
+++ src/main/java-templates/com/clxcommunications/xms/Version.java
@@ -1,9 +1,23 @@
 package com.clxcommunications.xms;
 
+/**
+ * A container of some static information concerning this CLX SDK.
+ */
 public final class Version {
 
+	/**
+	 * The Maven group ID of this SDK.
+	 */
 	public static final String GROUP_ID = "${project.groupId}";
+
+	/**
+	 * The Maven artifact ID of this SDK.
+	 */
 	public static final String ARTIFACT_ID = "${project.artifactId}";
+
+	/**
+	 * The version of this SDK.
+	 */
 	public static final String VERSION = "${project.version}";
 
 }
