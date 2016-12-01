ba1a93077e72c0fb40c94139a67da058304a338e
Robert Helgesson <robert@chaitsa.com>
2016-12-01 17:01:08 +0100

    Add javadoc

--- src/main/java/com/clxcommunications/xms/ClxApi.java
+++ src/main/java/com/clxcommunications/xms/ClxApi.java
@@ -18,42 +18,103 @@ import com.clxcommunications.xms.api.TagsUpdate;
  */
 public final class ClxApi { // TODO Maybe rename this to, e.g., "ApiBuilders"?
 
+	/**
+	 * Returns a freshly created batch SMS builder for text messages.
+	 * 
+	 * @return a builder of text batch messages
+	 */
+	@Nonnull
 	public static MtBatchTextSmsCreate.Builder batchTextSms() {
 		return new MtBatchTextSmsCreate.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder for text message updates.
+	 * 
+	 * @return a builder of text message updates
+	 */
+	@Nonnull
 	public static MtBatchTextSmsUpdate.Builder batchTextSmsUpdate() {
 		return new MtBatchTextSmsUpdate.Builder();
 	}
 
+	/**
+	 * Returns a freshly created batch SMS builder for binary messages.
+	 * 
+	 * @return a builder of binary batch messages
+	 */
+	@Nonnull
 	public static MtBatchBinarySmsCreate.Builder batchBinarySms() {
 		return new MtBatchBinarySmsCreate.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder for binary message updates.
+	 * 
+	 * @return a builder of binary message updates
+	 */
+	@Nonnull
 	public static MtBatchBinarySmsUpdate.Builder batchBinarySmsUpdate() {
 		return new MtBatchBinarySmsUpdate.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder of text message parameter
+	 * substitutions.
+	 * 
+	 * @return a builder of text parameter substitutions
+	 */
+	@Nonnull
 	public static ParameterValues.Builder parameterValues() {
 		return new ParameterValues.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder of groups.
+	 * 
+	 * @return a builder of groups
+	 */
+	@Nonnull
 	public static GroupCreate.Builder groupCreate() {
 		return new GroupCreate.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder of group updates.
+	 * 
+	 * @return a builder of group updates
+	 */
+	@Nonnull
 	public static GroupUpdate.Builder groupUpdate() {
 		return new GroupUpdate.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder of batch filters.
+	 * 
+	 * @return a builder of batch filters
+	 */
+	@Nonnull
 	public static BatchFilter.Builder batchFilter() {
 		return new BatchFilter.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder of group filters.
+	 * 
+	 * @return a builder of group filters
+	 */
+	@Nonnull
 	public static GroupFilter.Builder groupFilter() {
 		return new GroupFilter.Builder();
 	}
 
+	/**
+	 * Returns a freshly created builder of inbound message filters.
+	 * 
+	 * @return a builder of inbound message filters
+	 */
+	@Nonnull
 	public static InboundsFilter.Builder inboundsFilter() {
 		return new InboundsFilter.Builder();
 	}
