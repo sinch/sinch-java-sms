754cd0c9bffd980234dc2de643fd58a2c4bb852a
Gustav <gustav.svensk@sinch.com>
2022-05-23 15:05:58 +0200

    MSLRES-4940 Remove unnecessary flag

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ set -euo
 [ -z "$GPG_PASSPHRASE" ] && echo "Need to set GPG_PASSPHRASE" && exit 1;
 
 mvn release:clean
-mvn release:prepare --batch-mode "-Darguments=-DskipTests -DuseInternalRepo=true -Ddependency-check.skip=true"
+mvn release:prepare --batch-mode "-Darguments=-DskipTests -Ddependency-check.skip=true"
 mvn release:perform "-Darguments=-DskipTests -DuseInternalRepo=true"
