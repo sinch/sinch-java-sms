64374675339f99600e20cc31a7156164c0d6b04e
Gustav <gustav.svensk@sinch.com>
2022-05-24 10:29:18 +0200

    MSLRES-4940 Improve readability and UX of bash script

--- release.sh
+++ release.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-set -euo
+set -eu
 
 INTERNAL_RELEASE=true
 
+# Parses command line arguments
+# If called with -p, a public release is made
+# If called with -h, help text is displayed
+# If called without any arg, an internal release is made
 while getopts 'ph' OPTION; do
   case "$OPTION" in
     p)
-      echo "Making public release"
       INTERNAL_RELEASE=false
       ;;
     ?)
@@ -16,15 +19,19 @@ while getopts 'ph' OPTION; do
       ;;
   esac
 done
-shift "$(($OPTIND -1))"
+shift "$((OPTIND -1))"
 
+# Checks that required environment variables are set
 [ -z "$REPO_URL" ] && echo "Need to set REPO_URL" && exit 1;
 [ -z "$GPG_PASSPHRASE" ] && echo "Need to set GPG_PASSPHRASE" && exit 1;
 
 if [ "$INTERNAL_RELEASE" = true ] ; then
   echo "Making internal release"
+else
+  echo "Making public release"
 fi
 
+# Make release
 mvn release:clean
 mvn release:prepare --batch-mode "-Darguments=-DskipTests -Ddependency-check.skip=true"
 mvn release:perform "-Darguments=-DskipTests -DuseInternalRepo=${INTERNAL_RELEASE}"
